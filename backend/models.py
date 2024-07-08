8f8833896a27e3fad47e9850515f37d00b4cc3af
Ken_sir <KLOW024@e.ntu.edu.sg>
2024-07-08 14:33:07 +0800

    feat: Add User model to backend
    This commit adds a new User model to the backend, including the necessary database columns for id, username, email, and password.

--- backend/models.py
+++ backend/models.py
@@ -30,3 +30,23 @@ class Booking(db.Model):
     def update(self,title,description):
         self.title=title
         self.description=description
+
+
+'''
+class User:
+    id:int primary key
+    username:str
+    email:str
+    password:str
+'''
+
+class User(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    username=db.Column(db.String(25),nullable=False)
+    email=db.Column(db.String(80),nullable=False)
+    password=db.Column(db.Text(),nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+    
+    
